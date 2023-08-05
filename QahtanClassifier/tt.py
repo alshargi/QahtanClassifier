d72ca464f5c78706b549d1253761cf05325a7aa4
Faisal ALSHARGI <alshargi@hotmail.de>
2023-08-05 19:39:46 -0400

    Delete tt.py

--- QahtanClassifier/tt.py
+++
@@ -1,1 +0,0 @@
-#from QahtanClassifier import get_pred_labelimport quhran_predictxx = ['عام ألفين وثلاثة وعشرين']maxlabel, details = quhran_predict.get_pred_label(xx)print(maxlabel)print(details)
