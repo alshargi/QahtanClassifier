a72c357ebbcc083d774141b3c97266231fb224d0
Faisal ALSHARGI <alshargi@hotmail.de>
2023-08-05 19:51:51 -0400

    Update __init__.py

--- QahtanClassifier/__init__.py
+++ QahtanClassifier/__init__.py
@@ -1,3 +1,3 @@
 
 
-from .quhran_predict import get_pred_label, getFlag, get_pred_mor, get_pred_LEvant, get_pred_main, get_pred_gulf, findres
+from .quhran_predict import get_pred_label, get_pred_mor, get_pred_LEvant, get_pred_main, get_pred_gulf, findres
